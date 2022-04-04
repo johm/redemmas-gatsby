bd13903f9ef3be58b42a892d31725d3e5db50c24
John Duda <john@manifestor.org>
2022-04-04 09:01:43 -0400

    fix a null error

--- src/pages/events/{Airtable.data__Slug}.js
+++ src/pages/events/{Airtable.data__Slug}.js
@@ -14,35 +14,43 @@ const EventTemplate = ({ data }) =>{
     const doc = data.airtable.data
     
     return (
-
+	
 	<InnerLayout>
-
-	    <SEO title={doc.name} image={getSrc(doc.Image)} />	
-
-	    <div className="mb-6 xl:mr-48 xl:ml-48"  >
-		<GatsbyImage
-		    image={doc.Image &&
-			   doc.Image.localFiles[0].childImageSharp.gatsbyImageData
-		    }
-		    alt={doc.Name} className="mb-6 w-full "
-		/>
-		<div className="lg:ml-24 lg:mr-24 ">
-		    <h2 className="text-3xl md:text-4xl mb-3 font-text leading-tight text-stone-900">{doc.Name}</h2>
-		    <div className="flex flex-wrap mb-6">
-			<div className="w-full md:w-1/2 font-subhed uppercase text-xl">
-			    <div>{moment(doc.Date_and_time).format("dddd, MMMM Do YYYY")}</div>
-			    <div>{moment(doc.Date_and_time).format("h:mm a")}</div>
-			    <div>{doc.Location}</div>
-			</div>
-			<div className="w-full md:w-1/2">
-			    {doc.Withfriends_url &&  <a href={doc.Withfriends_url} className="inline-block mt-1 text-center  rounded-full bg-violet-900 text-stone-100 font-subhed px-3 py-0.5 uppercase hover:bg-stone-800 transition-colors text-xl w-full">RSVP on withfriends</a>}
-			</div>
-		    </div>
-
-
-		    <div className="font-text text-2xl mb-3">{doc.Short_Description}</div>
-		    <div className="font-text mb-3"><Markdown>{doc.Long_Description}</Markdown></div>
-		    <div className="font-text mb-3"><Markdown>{doc.Author_bio}</Markdown></div>
+	
+	<SEO title={doc.name} image={getSrc(doc.Image)} />	
+	
+	<div className="mb-6 xl:mr-48 xl:ml-48"  >
+	<GatsbyImage
+	image={doc.Image &&
+	       doc.Image.localFiles[0].childImageSharp.gatsbyImageData
+	}
+	alt={doc.Name} className="mb-6 w-full "
+	/>
+	<div className="lg:ml-24 lg:mr-24 ">
+	<h2 className="text-3xl md:text-4xl mb-3 font-text leading-tight text-stone-900">{doc.Name}</h2>
+	<div className="flex flex-wrap mb-6">
+	<div className="w-full md:w-1/2 font-subhed uppercase text-xl">
+	<div>{moment(doc.Date_and_time).format("dddd, MMMM Do YYYY")}</div>
+	<div>{moment(doc.Date_and_time).format("h:mm a")}</div>
+	<div>{doc.Location}</div>
+	</div>
+	<div className="w-full md:w-1/2">
+	{doc.Withfriends_url &&  <a href={doc.Withfriends_url} className="inline-block mt-1 text-center  rounded-full bg-violet-900 text-stone-100 font-subhed px-3 py-0.5 uppercase hover:bg-stone-800 transition-colors text-xl w-full">RSVP on withfriends</a>}
+	</div>
+	</div>
+	
+	
+	<div className="font-text text-2xl mb-3">{doc.Short_Description}</div>
+	<div className="font-text mb-3">
+	{doc.Long_Description &&
+	 <Markdown>{doc.Long_Description}</Markdown>
+	}
+	</div>
+	<div className="font-text mb-3">
+	    {doc.Author_bio && 
+	     <Markdown>{doc.Author_bio}</Markdown>
+	    }
+	</div>
 
 		    
 		    {doc.Withfriends_url &&  <a href={doc.Withfriends_url} className="inline-block mt-1 mb-3 text-center  rounded-full bg-violet-900 text-stone-100 font-subhed px-3 py-0.5 uppercase hover:bg-stone-800 transition-colors text-xl w-full">RSVP on withfriends</a>}
