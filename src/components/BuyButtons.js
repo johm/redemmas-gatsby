bfc3d8eb8db79bdc51cc043cf1de708ff23c5695
John Duda <john@manifestor.org>
2022-04-24 22:13:59 -0400

    new location

--- src/components/BuyButtons.js
+++ src/components/BuyButtons.js
@@ -21,10 +21,10 @@ const BuyButtons = ({title,edition}) => {
 	      <button className="w-full snipcart-add-item rounded-full bg-red-400 text-stone-100 font-subhed px-3 py-0.5 uppercase hover:bg-stone-800 transition-colors duration-500"
 		      data-item-id={edition.key}
 		      data-item-price={edition.list_price}
-		      data-item-url={"https://redemmasgatsbymain.gtsb.io/titles/"+title.slug}
+		      data-item-url={"https://redemmas.org/titles/"+title.slug}
 		      data-item-weight="750"	
 		      data-item-description={edition.isbn13}
-		      data-item-image={'https://redemmas.org'+ edition.cover_image_url}
+		      data-item-image={'https://old.redemmas.org'+ edition.cover_image_url}
 		      data-item-name={title.title}>
 		  {instock === "IN STOCK" &&  <>Add to cart</>}
 		  {instock === "PREORDER" &&  <>Preorder</>}
