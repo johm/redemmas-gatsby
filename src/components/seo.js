94c31be860544fa86f8701c3e72a9d4cb2d5ec1d
John Duda <john@manifestor.org>
2022-05-14 21:02:04 -0400

    remove double slash to make twitter happy

--- src/components/seo.js
+++ src/components/seo.js
@@ -25,39 +25,38 @@ const SEO = ({ title, description, image, article }) => {
   }
 
   return (
-    <Helmet title={seo.title} titleTemplate={titleTemplate}>
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
-      <link rel="preconnect" href="https://app.snipcart.com" />
-      <link rel="preconnect" href="https://cdn.snipcart.com" />
-      <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.css" />
+      <Helmet title={seo.title} titleTemplate={titleTemplate}>
+	  <meta name="description" content={seo.description} />
+	  <meta name="image" content={seo.image} />
+	  <link rel="preconnect" href="https://app.snipcart.com" />
+	  <link rel="preconnect" href="https://cdn.snipcart.com" />
+	  <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.3.0/default/snipcart.css" />
+	  <meta name="twitter:card" content="summary_large_image" />
+	  		  {seo.url && <meta property="og:url" content={seo.url} />}
 
-      {seo.url && <meta property="og:url" content={seo.url} />}
+		  {(article ? true : null) && <meta property="og:type" content="article" />}
 
-      {(article ? true : null) && <meta property="og:type" content="article" />}
+		  {seo.title && <meta property="og:title" content={seo.title} />}
 
-      {seo.title && <meta property="og:title" content={seo.title} />}
+		  {seo.description && (
+		      <meta property="og:description" content={seo.description} />
+		  )}
 
-      {seo.description && (
-        <meta property="og:description" content={seo.description} />
-      )}
+		  {seo.image && <meta property="og:image" content={seo.image} />}
 
-      {seo.image && <meta property="og:image" content={seo.image} />}
 
-      <meta name="twitter:card" content="summary_large_image" />
+		  {twitterUsername && (
+		      <meta name="twitter:creator" content={twitterUsername} />
+		  )}
 
-      {twitterUsername && (
-        <meta name="twitter:creator" content={twitterUsername} />
-      )}
+		  {seo.title && <meta name="twitter:title" content={seo.title} />}
 
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
+		  {seo.description && (
+		      <meta name="twitter:description" content={seo.description} />
+		  )}
 
-      {seo.description && (
-        <meta name="twitter:description" content={seo.description} />
-      )}
-
-      {seo.image && <meta name="twitter:image" content={seo.image} />}
-    </Helmet>
+		  {seo.image && <meta name="twitter:image" content={seo.image} />}
+      </Helmet>
   )
 }
 
