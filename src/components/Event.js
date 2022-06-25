795f2fe1077fff6257ed47f30d8208391faa0d68
John Duda <johnduda@Johns-MacBook-Pro-2.local>
2022-06-25 09:49:05 -0400

    time zone madness

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,8 +1,8 @@
 import React from "react"
-import moment from 'moment'
+import moment from 'moment-timezone'
 import { GatsbyImage} from "gatsby-plugin-image";
 import { Link } from "gatsby"
-
+ 
 
 const Event = ({e}) => (
 
@@ -19,8 +19,8 @@ const Event = ({e}) => (
 	</Link>
 	     
 	<a href={"/events/"+e.Slug}><h2 className="text-2xl md:text-3xl mb-3 font-text leading-tight text-stone-900">{e.Name}</h2></a>
-	<div className="font-subhed uppercase">{moment(e.Date_and_time).format("dddd, MMMM Do YYYY")}</div>
-	<div className="font-subhed uppercase">{moment(e.Date_and_time).format("h:mm a")}</div>
+	<div className="font-subhed uppercase">{moment(e.Date_and_time).tz('America/New_York').format("dddd, MMMM Do YYYY")}</div>
+	<div className="font-subhed uppercase">{moment(e.Date_and_time).tz('America/New_York').format("h:mm a")}</div>
 	<div className="font-subhed uppercase">{e.Location}</div>
 	{e.Withfriends_url &&  <a href={e.Withfriends_url} className="inline-block mt-1 text-center  rounded-full bg-violet-900 text-stone-100 font-subhed px-3 py-0.5 uppercase hover:bg-stone-800 transition-colors text-xs">RSVP on withfriends</a>}
 
--- src/components/ShortEvent.js
+++ src/components/ShortEvent.js
@@ -1,5 +1,5 @@
 import React from "react"
-import moment from 'moment'
+import moment from 'moment-timezone'
 import { GatsbyImage} from "gatsby-plugin-image";
 import { Link } from "gatsby"
 
@@ -17,8 +17,8 @@ const ShortEvent = ({e}) => (
 	    />
 	    
 	    <h2 className="text-2xl md:text-3xl mb-3 font-text leading-tight text-stone-900">{e.Name}</h2></Link>
-	<div className="font-subhed uppercase">{moment(e.Date_and_time).format("dddd, MMMM Do YYYY")}</div>
-	<div className="font-subhed uppercase">{moment(e.Date_and_time).format("h:mm a")}</div>
+	<div className="font-subhed uppercase">{moment(e.Date_and_time).tz('America/New_York').format("dddd, MMMM Do YYYY")}</div>
+	<div className="font-subhed uppercase">{moment(e.Date_and_time).tz('America/New_York').format("h:mm a")}</div>
 	<div className="font-subhed uppercase mb-6">{e.Location}</div>
 	{e.Withfriends_url &&  <a href={e.Withfriends_url} className="inline-block mt-1 absolute bottom-0 text-center  rounded-full bg-violet-900 text-stone-100 font-subhed px-3 py-0.5 uppercase hover:bg-stone-800 transition-colors text-xs">RSVP on withfriends</a>}
     </div>
