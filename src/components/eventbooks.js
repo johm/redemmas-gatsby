194bcbcf6a703a9db3bb802f802953b0e0885b7a
John Duda <johnduda@Johns-MBP-4.hsd1.md.comcast.net>
2023-09-16 11:44:34 -0400

    fixing

--- src/components/eventbooks.js
+++ src/components/eventbooks.js
@@ -9,27 +9,25 @@ const EventBook = ({isbn}) => {
     const [resultData, setResultData] = useState(false)
 
     useEffect(() => {
-	fetch(`${process.env.GATSBY_INVENTORY_SERVER}/titles.json?searchquery=${formData.query}`)
+	fetch(`${process.env.GATSBY_INVENTORY_SERVER}/titles.json?searchquery=${isbn}`)
 	 .then(response => response.json()) // parse JSON from request
-	 .then(responseData => {
-	     setResultData({...resultData,results: responseData})
-	 }) 
+	    .then(responseData => {
+		setResultData({...resultData,results: responseData})
+	    }) 
     }, [])
     
     return (
-
-
-	
-		{resultData.results.map ((result,index) => {
-		    return (
-			<Book title={result.the_title} edition={result.the_edition} />
-		    )
-		}
-					)
-		}
-	
+	<>
+	    {resultData.results.map ((result,index) => {
+		return (
+		    <Book title={result.the_title} edition={result.the_edition} />
+		)
+	    }
+				    )
+	    }
+	</>
     )
-	}
+}
 
 export default EventBook
 
