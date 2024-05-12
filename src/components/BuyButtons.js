0614306dcc154cda95901ef06a4e1ac7001f87ff
John Duda <johnduda@Johns-MacBook-Pro-4.local>
2024-05-12 17:55:42 -0400

    fixing snipcart issue

--- src/components/BuyButtons.js
+++ src/components/BuyButtons.js
@@ -17,7 +17,24 @@ const BuyButtons = ({title,edition}) => {
 
     return (
 	<>
-
+	    { (! instock &&
+<button className="w-full snipcart-add-item rounded-full bg-stone-100 text-stone-300 font-subhed px-3 py-0.5 uppercase "
+		      data-item-id={edition.key}
+		      data-item-price={edition.list_price}
+		      data-item-url={"https://redemmas.org/titles/"+title.slug}
+		      data-item-weight="750"	
+		      data-item-description={edition.isbn13}
+		      data-item-image={'https://old.redemmas.org'+ edition.cover_image_url}
+		      data-item-categories="notloadedyet" 
+		      data-item-name={ title.title }>
+<svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+    CHECKING INVENTORY 
+	      </button>
+	      }
+	    
 	{ (instock !== "UNAVAILABLE" && instock !== "OUT OF PRINT" && instock ) && 
 	      <button className="w-full snipcart-add-item rounded-full bg-red-400 text-stone-100 font-subhed px-3 py-0.5 uppercase hover:bg-stone-800 transition-colors duration-500"
 		      data-item-id={edition.key}
