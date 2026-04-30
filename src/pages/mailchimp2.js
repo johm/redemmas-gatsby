40e2f90a93989e072c9fe69c5997c517e0781b5f
John Duda <john.f.duda@gmail.com>
2026-04-30 19:54:40 -0400

    mailchimp update

---
+++ src/pages/mailchimp2.js
@@ -0,0 +1,172 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import React, {useState,useEffect} from "react"
+import { graphql, useStaticQuery, Link } from 'gatsby'
+
+import { renderToStaticMarkup } from 'react-dom/server'
+
+
+import SEO from "../components/seo"
+import InnerLayout from "../components/InnerLayout.js"
+import moment from 'moment'
+import Event from "../components/Event"
+
+
+const EventsPage = () => {
+
+    
+    const eventsData = useStaticQuery(graphql`{
+  allAirtable(
+    sort: {data: {Date_and_time: ASC}}
+    filter: {table: {eq: "Events"}, data: {Status: {eq: "Published"}, Upcoming: {eq: 1}, WBFOnly: {ne: true}}}
+  ) {
+    edges {
+      node {
+        data {
+          Name
+          Slug
+          Date_and_time
+          Location
+          List_in_Courses_and_meet_ups
+          Short_Description
+          Author_bio
+          Withfriends_url
+          Image {
+            localFiles {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+}`)
+
+    
+    return (
+	<InnerLayout>
+
+
+	    <h1 className="text-4xl md:text-6xl mb-6 font-text text-stone-900 border-b border-yellow-700">Upcoming events</h1>
+		<textarea id="mytextarea" className="w-full h-screen">
+		    {renderToStaticMarkup(<Evs eventsData={eventsData}/>)}
+		</textarea>
+
+	</InnerLayout>
+    )
+}
+
+const Ev = ({ev}) => {
+    return (
+    	<>
+		    <div key={ev.Slug} className="mcnTextContent" style="padding:0px;margin-top:20px;">
+			<a href={"https://redemmas.org/events/"+ev.Slug}>
+			    <img width="100%" src={"https://redemmas.org"+ encodeURI(ev.Image.localFiles[0].publicURL)} />
+			</a>
+			<br /><br />
+			<h3 style="padding-right:10px;padding-left:10px;"><a style="text-decoration:none !important;" href={"https://redemmas.org/events/"+ev.Slug}>{ev.Name}</a></h3>
+			<table style="margin-top:10px;">
+			    <tr>
+				<td width="60%">{ev.Short_Description}</td>
+				<td width="40%" style="text-align:right;">
+				    <h4 className="">{moment(ev.Date_and_time).tz('America/New_York').format("ddd MMMM D")}</h4>
+				    <h4 className="">{moment(ev.Date_and_time).tz('America/New_York').format("h:mm a")}</h4>
+				    <h4 className="">{ev.Location}</h4>
+				    {ev.Withfriends_url &&  <a href={ev.Withfriends_url} className="" style={{
+								   fontFamily:"Helvetica",
+								   border:"2px solid #4c1d95",
+								   color:"#4c1d95",
+								   padding:"10px",
+								   paddingRight:"20px",
+								   paddingLeft:"20px",
+								   textDecoration:"none",
+ 				  				   borderRadius:"10px"}} >RSVP now!</a>}
+				</td>
+			    </tr>
+			</table>
+
+
+
+
+		    </div>
+	 		{`\n\n`}
+	    </>)
+};
+
+const ShortEv = ({ev}) => {
+    return (
+    	    <>
+		    <div key={ev.Slug} className="mcnTextContent">
+			<h3><a href={"https://redemmas.org/events/"+ev.Slug}>{ev.Name}</a></h3>
+			<br />
+			<h4 className="">{moment(ev.Date_and_time).tz('America/New_York').format("dddd, MMMM Do YYYY")}</h4>
+			<h4 className="">{moment(ev.Date_and_time).tz('America/New_York').format("h:mm a")}</h4>
+			<h4 className="">{ev.Location}</h4>
+
+			{ev.Withfriends_url && 		<><br /> <a href={ev.Withfriends_url} className="" style={{backgroundColor:"#4c1d95",
+												  color:"#ffffff",
+												  padding:"3px",
+												  paddingRight:"10px",
+												  paddingLeft:"10px",
+												  borderRadius:"10px"}} >RSVP on withfriends</a> </>}
+			<br /><br />
+			<div className="font-text">{ev.Short_Description}</div>
+						<br />
+		    </div>
+	 		{`\n\n`}
+	    </>)
+};
+
+
+
+const Evs = ({eventsData})=>{
+
+    return (
+	<>
+	    {eventsData.allAirtable.edges.filter(edge => ! edge.node.data.List_in_Courses_and_meet_ups).slice(0,2).map((e,index) => {
+		return (
+		    <Ev ev={e.node.data} />
+		)}
+					     )}
+
+	    <table style={{backgroundColor:"#e2e8f0"}}>
+	<tr><td style={{padding:"20px"}}>
+	<h4 style={{marginBottom:"20px"}}>Courses, workshops, and meetups...</h4>
+	<br />
+	{eventsData.allAirtable.edges.filter(edge => edge.node.data.List_in_Courses_and_meet_ups).slice(0,2).map((e,index) => {
+		return (
+		    <ShortEv ev={e.node.data} />
+		)}
+					     )}
+	    </td></tr></table>
+	    <br />
+	    {eventsData.allAirtable.edges.filter(edge => ! edge.node.data.List_in_Courses_and_meet_ups).slice(2,6).map((e,index) => {
+		return (
+		    <Ev ev={e.node.data} />
+		)}
+														      )}
+	    <table style={{backgroundColor:"#e2e8f0"}}>
+	<tr><td style={{padding:"20px"}}>
+		<h4 style={{marginBottom:"20px"}}>More courses, workshops, and meetups...</h4> <br />
+<br />
+	{eventsData.allAirtable.edges.filter(edge => edge.node.data.List_in_Courses_and_meet_ups).slice(2,4).map((e,index) => {
+		return (
+		    <ShortEv ev={e.node.data} />
+		)}
+					     )}
+</td></tr></table>
+<br />
+	    {eventsData.allAirtable.edges.filter(edge => ! edge.node.data.List_in_Courses_and_meet_ups).slice(6).map((e,index) => {
+		return (
+		    <Ev ev={e.node.data} />
+		)})}
+	
+
+	    
+	</>
+    )};
+
+
+
+
+export default EventsPage;
